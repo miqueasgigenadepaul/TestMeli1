4451560a1f692cedb34275284aa4d9642dd7169b
Miqueas <miqueasgigena18@gmail.com>
2025-01-20 00:33:15 -0300

    tengo que agregar mas productos, entendi mal la consigna y pense que tenian que ser 4 productos en total, aparte de esto, no se si lo veo conveniente poner todas las imagenes como si fueran un boton, y tambien se me esta complicando usar grid a la hora de mostrar los detalles de cada producto

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -22,16 +22,23 @@ const ProductList = ({searchTerm}) => {
     product.info.toLowerCase().includes(searchTerm.toLowerCase())
     )
 
+    // muestra solamente el producto seleccionado
     const handleShowDetails = (product) => {
-        setSelectedProduct(product); // Actualiza el estado con el producto seleccionado
+        console.log('selected product', product)
+        setSelectedProduct(product) // Actualiza el estado con el producto seleccionado
+    }
+
+    // muestra la lista completa de productos
+    const handleCloseDetails = () => {
+        console.log('cerrar')
+        setSelectedProduct(null)
     }
 return (
         <div>
-            <ul className = "dataResult">
+            <ul className = {`dataResult ${selectedProduct ? "hidden" : ""}`}>
                 {filteredProducts.map(product => 
-
                     <li key={product.id} className = "items">
-                        <button onClick = {handleShowDetails}>
+                        <button onClick = {() => handleShowDetails(product)}>
                             <div className = "image-data-container">
                                 <img className = "productImage" src = {product.imageUrl}
                                     width = "100"
@@ -47,7 +54,19 @@ return (
             </ul>
 
             {/*mostrar detalles del producto seleccionado*/}
-            
+            <div className = "unique-product-data-result">
+            {selectedProduct && (
+                <div className="product-details">
+                    <img className = 'only-one-image' src = {selectedProduct.imageUrl}/>
+                    <p><strong>Nombre:</strong> {selectedProduct.info}</p>
+                    <h2><strong>Descripción del producto</strong></h2>
+                    <p class = "precio"><strong>Precio:</strong> {selectedProduct.price}</p>
+                    <p className = "product-description">{selectedProduct.description}</p>
+                    <button className = "buy-button">comprar</button>
+                    <button className = "close-button" onClick={handleCloseDetails}>Cerrar</button>
+                </div>
+            )}
+            </div>
         </div>
     )
 }
