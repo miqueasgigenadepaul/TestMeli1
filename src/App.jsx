0e79576c68d728ceaa22112df66ab7170e1cc9a5
Miqueas <miqueasgigena18@gmail.com>
2025-01-22 22:59:18 -0300

    subiendo todo version 1

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import {useState, useEffect} from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import ProductList from "./components/ProductList";
+
+// caja de busqueda
+
+const App = () => {
+  
+  const [searchTerm, setSearchTerm] = useState('')
+  const [triggerSearch, setTriggerSearch] = useState(false)
+
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value)
+}
+
+
+  const handleSearchClick = (e) => {
+    e.preventDefault();
+    setTriggerSearch(true);
+}
+
+  return (
+    <div>
+      <SearchBar searchTerm = {searchTerm} 
+                handleSearchChange = {handleSearchChange}
+                handleSearchClick = {handleSearchClick}
+      />
+      <ProductList searchTerm = {searchTerm}
+        triggerSearch = {triggerSearch}
+        resetSearch = {() => setTriggerSearch(false)}
+      />
+    </div>
+  )
+}
+
+export default App
+
+
